9b72e93b88afd8a2e12d21306810ec06eb178f69
rcardiff1 <robertstuartcardiff@gmail.com>
2016-01-28 15:21:09 -0800

    [refactor] edit font link tags in index.html

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -3,7 +3,12 @@ var bodyParser = require('body-parser');
 var jwt = require('express-jwt');
 // var config = require('./../env/config.js');
 
-module.exports = function(app, express, jwtCheck){
+module.exports = function(app, express){
+
+  var jwtCheck = jwt({
+    secret: new Buffer(process.env.AUTH0_CLIENT_SECRET),
+    audience: '909iKBxjdoJbblSUwtwFKFwfkqZss09d'
+  });
 
 	app.use(morgan('dev'));
 	app.use(bodyParser.json());
--- server/server.js
+++ server/server.js
@@ -3,14 +3,9 @@ var port = process.env.PORT || 3000;
 var app = express();
 var jwt = require('express-jwt');
 
-var jwtCheck = jwt({
-  secret: new Buffer(process.env.AUTH0_CLIENT_SECRET),
-  audience: '909iKBxjdoJbblSUwtwFKFwfkqZss09d'
-});
-
 var middleware = require('./config/middleware.js');
 
-middleware(app, express, jwtCheck);
+middleware(app, express);
 
 app.listen(port);
 console.log('listening on ' + port);
